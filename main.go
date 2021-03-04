b751af09224831d67d4e504f154eaf3b94b6351d
ChenTanyi <chentanyi@gmail.com>
2021-03-04 18:05:44 +0800

    feat: list all keys in the web

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sort"
 	"strings"
 	"sync"
 	"text/template"
@@ -10,41 +11,71 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Message string
-
-func (m Message) Message() string {
-	return string(m)
+type Message struct {
+	message string
+	keys    []string
 }
 
-var messages = make(map[string]Message)
+func (m *Message) Message() string {
+	return m.message
+}
+
+func (m *Message) Keys() []string {
+	return m.keys
+}
+
+var messages = make(map[string]string)
 var messageLock = &sync.RWMutex{}
 
+func extractKeys(m map[string]string, columnSize int) []string {
+	keys := make([]string, 0, len(m))
+	for key := range m {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
 func messageHandler(c *gin.Context) {
-	receive := strings.TrimSpace(c.Query("message"))
 	path := c.Request.URL.Path
 	if path[len(path)-1] != '/' {
 		c.Redirect(307, fmt.Sprintf("%s/?%s", path, c.Request.URL.RawQuery))
 		return
 	}
 
-	if receive != "" {
-		messageLock.Lock()
-		defer messageLock.Unlock()
-		if len(messages) > 1000 {
-			messages = make(map[string]Message)
-		}
-		messages[path] = Message(receive)
-
-		query := c.Request.URL.Query()
-		query.Del("message")
-		c.Redirect(307, fmt.Sprintf("./?%s", query.Encode()))
-		return
+	if c.Request.Method == "POST" {
+		messagePost(c, path)
+	} else {
+		messageGet(c, path)
 	}
+}
+
+func messagePost(c *gin.Context, path string) {
+	messageLock.Lock()
+	defer messageLock.Unlock()
+
+	receive := strings.TrimSpace(c.PostForm("message"))
+	if receive != "" {
+		if len(messages) > 1000 {
+			messages = make(map[string]string)
+		}
+		messages[path] = receive
+	} else {
+		delete(messages, path)
+	}
+
+	c.Redirect(303, "./")
+}
+
+func messageGet(c *gin.Context, path string) {
 	messageLock.RLock()
 	defer messageLock.RUnlock()
 
 	tpl := template.Must(template.New("Message").Parse(string(MustAsset("template/message.gohtml"))))
-	message, _ := messages[path]
+	message := &Message{
+		message: messages[path],
+		keys:    extractKeys(messages, 5),
+	}
 	if err := tpl.Execute(c.Writer, message); err != nil {
 		fmt.Println(err)
 	}
